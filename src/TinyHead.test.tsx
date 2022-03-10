dffb3281d507216a92b053b7df91c459f5469487
Diogo Parente <66437099+diogoparente@users.noreply.github.com>
2022-03-10 16:59:28 +0000

    Add TinyHead (#2)
    * Add TinyHead
    
    This component will help consuming apps when handling a TinyFrontendSsrConfig
    response and building head elements.
    
    Its utility is to build the elements instead of the user having to do it.
    
    * Add global jest setup
    
    * Move tests closer to implementation
    
    Co-authored-by: Diogo Parente <diogo.parente@cazoo.co.uk>

---
+++ src/TinyHead.test.tsx
@@ -0,0 +1,66 @@
+import { render } from "@testing-library/react";
+import { TinyFrontendSsrConfig } from "@tiny-frontend/client";
+import React from "react";
+
+import { TinyHead } from "./TinyHead";
+
+const renderTinyHead = (config?: Partial<TinyFrontendSsrConfig>) => {
+  const mergedConfig = {
+    jsBundle: "http://some-js-url.xyz/",
+    moduleConfigScript: `window["tinyFrontendFrontendConfig"] = {"umdBundle":"some-umd.js","cssBundle":"some-css.css"}`,
+    cssBundle: "http://some-css-url.xyz/",
+    ...config,
+  };
+
+  return render(<TinyHead config={mergedConfig} />);
+};
+
+describe("[TinyHead]", () => {
+  describe("cssBundle", () => {
+    it("should render a link with the provided cssBundle", () => {
+      const expected = "http://other-css-url.css/";
+      const { container } = renderTinyHead({ cssBundle: expected });
+
+      const link = container.querySelector(
+        "link[rel=stylesheet]"
+      ) as HTMLLinkElement;
+      const result = link.href;
+
+      expect(result).toBe(expected);
+    });
+
+    describe("when there is not a cssBundle", () => {
+      it("should not render a link with the provided cssBundle", () => {
+        const { container } = renderTinyHead({ cssBundle: undefined });
+
+        const result = container.querySelector(
+          "link[rel=stylesheet]"
+        ) as HTMLLinkElement;
+
+        expect(result).toBe(null);
+      });
+    });
+  });
+  describe("jsBundle", () => {
+    it("should render a link with the provided jsBundle", () => {
+      const expected = "http://other-js-url.js/";
+      const { container } = renderTinyHead({ jsBundle: expected });
+
+      const link = container.querySelector("link[as=fetch]") as HTMLLinkElement;
+      const result = link.href;
+
+      expect(result).toBe(expected);
+    });
+  });
+  describe("moduleConfigScript", () => {
+    it("should render a script with the provided module config script", () => {
+      const expected = `window["anotherTinyFrontendFrontendConfig"] = {"umdBundle":"some-other-umd.js","cssBundle":"some-other-css.css"}`;
+      const { container } = renderTinyHead({ moduleConfigScript: expected });
+
+      const link = container.querySelector("script") as HTMLScriptElement;
+      const result = link.textContent;
+
+      expect(result).toBe(expected);
+    });
+  });
+});
--- src/withHydrationSuppress.test.tsx
+++ src/withHydrationSuppress.test.tsx
@@ -1,9 +1,7 @@
-import "@testing-library/jest-dom/extend-expect";
-
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import { withHydrationSuppress } from "../withHydrationSuppress";
+import { withHydrationSuppress } from "./withHydrationSuppress";
 
 const renderWithHydrationSuppress = (loader) => {
   const Component = withHydrationSuppress(loader);
