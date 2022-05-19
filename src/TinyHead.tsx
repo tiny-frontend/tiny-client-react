c71e803bbfbf6c62802b9679965c8ee0d1d5c0da
Adrien Baron <adrien@abaron.net>
2022-05-19 15:59:50 +0200

    fix: remove crossOrigin attribute from script preload (#7)

--- src/TinyHead.tsx
+++ src/TinyHead.tsx
@@ -10,7 +10,7 @@ const TinyHead: React.FC<Props> = ({ config }) => {
   return (
     <>
       {cssBundle ? <link rel="stylesheet" href={cssBundle} /> : null}
-      <link rel="preload" href={jsBundle} as="script" crossOrigin="anonymous" />
+      <link rel="preload" href={jsBundle} as="script" />
       <script dangerouslySetInnerHTML={{ __html: moduleConfigScript }} />
     </>
   );
