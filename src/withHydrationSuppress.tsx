173f54fbcfe67c2f80e69c8208ccb7c3e2750fda
Maria Kozłowska <30720963+merykozlowska@users.noreply.github.com>
2022-08-17 23:22:46 +0100

    fix: add root element to withHydrationSuppress (#8)

--- src/withHydrationSuppress.tsx
+++ src/withHydrationSuppress.tsx
@@ -6,26 +6,28 @@ import { useBundle } from "./useBundle";
 // The withHydrationSuppress HOC will prevent React from hydrating any SSR HTML until a client side
 // component, received from a bundle, is made available.
 
-// This achieved by falling back to an empty element with suppressHydrationWarning enabled.
+// This is achieved by falling back to an empty element with suppressHydrationWarning enabled.
 // You can find the documentation for suppressHydrationWarning at https://reactjs.org/docs/dom-elements.html#suppresshydrationwarning.
+// RootElement has to be the same element type as the root of the loaded component!
 
 // We got the idea from https://github.com/valcol/react-hydration-on-demand, but as the library provided
 // many other capabilities that we don't require and also a big package dependency, we ported just the
 // needed code.
 
 export const withHydrationSuppress = function <T>(
-  loader: BundleLoader<ComponentType<T>>
+  loader: BundleLoader<ComponentType<T>>,
+  RootElement: keyof JSX.IntrinsicElements
 ) {
   // eslint-disable-next-line react/display-name
   return ({ ...props }: T) => {
     const Component = useBundle<ComponentType<T>>({
-      loader
+      loader,
     });
 
     return Component ? (
       <Component {...props} />
     ) : (
-      <div
+      <RootElement
         dangerouslySetInnerHTML={{ __html: "" }}
         suppressHydrationWarning={true}
       />
